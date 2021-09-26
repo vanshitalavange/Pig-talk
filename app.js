c9231fa53a6d8ba4dffd282759d65f5edb16667f
vanshitalavange <vanshita.a.lavange@gmail.com>
2021-09-27 03:43:37 +0530

    js added

---
+++ app.js
@@ -0,0 +1,25 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var textInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+var serverURL = "https://api.funtranslations.com/translate/pig-latin.json";
+
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("Server is down. Try again after some time");
+}
+function clickHandler() {
+  var inputText = textInput.value;
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler);
+}
+btnTranslate.addEventListener("click", clickHandler);
